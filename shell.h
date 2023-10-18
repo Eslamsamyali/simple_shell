8c56dae6ae09fa506bfaea855b986cd0304f1725
eslam <essamyali2@gmail.com>
2023-10-18 09:54:49 -0700

    git pull

--- shell.h
+++ shell.h
@@ -39,6 +39,6 @@ int _putchar(char);
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
-char **strtow)char *, char *);
+char **strtow(char *, char *);
 char **strtow2(char *, char);
 #endif /* PROJECT_SHELL_H */
